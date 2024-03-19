7aae7c9d484fd6e5c261b263fc8ca68ce4519c8b
lorenmartin <162948011+lorenmartin@users.noreply.github.com>
2024-03-20 12:32:31 +1300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+let imgs = {};
+let orange, banana, pear;
+
+function preload(){
+  orange = loadImage('images/orange.jpg');
+  banana = loadImage('images/banana.jpg');
+  pear = loadImage('images/pear.jpg')
+}
+
+function setup(){
+  createCanvas(400, 400);
+  frameRate(1);
+  imgs.push(orange);
+  imgs.push(banana);
+  imgs.push(pear);
+}
+
+function draw(){
+  background(220);
+  let r = random(imgs);
+  image(r, 0, 0);
+}
